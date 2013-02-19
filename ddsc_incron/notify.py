c199878c667f5a1a9e902ad251b6ce5f3bc06abf
shaoqing <shaoqing@ldam-fgslinux.(none)>
2013-02-19 15:07:33 +0100

    filled with real life script

--- ddsc_incron/notify.py
+++ ddsc_incron/notify.py
@@ -1,8 +1,9 @@
 from ddsc_incron.celery import celery
-
+import sys
 
 def main():
-    celery.send_task("ddsc_worker.tasks.add", kwargs={'x': 4, 'y': 4})
+    celery.send_task("ddsc_worker.importer.new_file_detected", \
+        kwargs={'pathDir': (sys.argv[1] + '/'), 'fileName': sys.argv[2]})
 
 if __name__ == "__main__":
     main()
